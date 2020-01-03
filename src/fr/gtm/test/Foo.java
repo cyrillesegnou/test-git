580e2d5a5343b15979a33b9681fba3ce2965d333
adminl <adminl@localhost>
2020-01-03 15:11:37 +0100

    modification getters and setters

--- src/fr/gtm/test/Foo.java
+++ src/fr/gtm/test/Foo.java
@@ -1,5 +1,20 @@
 package fr.gtm.test;
 
 public class Foo {
+	private int j = 5;
+	private int z = 5 ;
+	public int getZ() {
+		return z;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	
 
 }
